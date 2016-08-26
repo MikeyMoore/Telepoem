1cb03a38f08dd792237ea9a084a128b9e77aac50
MikeyMoore <icarusjedi@gmail.com>
2016-08-26 15:29:36 -0700

    View all pieces, show one piece, and have page to edit piece

--- telepoem/app/controllers/posts_controller.rb
+++ telepoem/app/controllers/posts_controller.rb
@@ -1,9 +1,17 @@
 class PostsController < ApplicationController
 	def index
-		if Posts.all != nil
-			@posts = Posts.all
+		if Post.all != nil
+			@posts = Post.all
 		else
 			@posts = "pop"
 		end
+	end
+
+	def show
+		@post = Post.find(params[:id])
+	end
+
+	def edit
+		@post = Post.find(params[:id])
 	end
 end
