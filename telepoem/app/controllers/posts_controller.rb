8ad7996e203b079274040a4e49100a776205aa02
MikeyMoore <icarusjedi@gmail.com>
2016-08-26 17:56:18 -0700

    Update render show page of post

--- telepoem/app/controllers/posts_controller.rb
+++ telepoem/app/controllers/posts_controller.rb
@@ -16,11 +16,8 @@ class PostsController < ApplicationController
 	end
 
 	def update
-		# raise params.inspect
 		@post = Post.find(params[:format])
 		@post.update(:content => params[:post][:content])
-		# p "Here is the content"
-		# p params[:post][:content]
-		redirect_to root_path
+		render "show"
 	end
 end
