9b8f5846069e38c503e6d24bf46cbd6d1ad2534d
MikeyMoore <icarusjedi@gmail.com>
2016-08-26 17:51:04 -0700

    Create update route

--- telepoem/app/controllers/posts_controller.rb
+++ telepoem/app/controllers/posts_controller.rb
@@ -14,4 +14,13 @@ class PostsController < ApplicationController
 	def edit
 		@post = Post.find(params[:id])
 	end
+
+	def update
+		# raise params.inspect
+		@post = Post.find(params[:format])
+		@post.update(:content => params[:post][:content])
+		# p "Here is the content"
+		# p params[:post][:content]
+		redirect_to root_path
+	end
 end
